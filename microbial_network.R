873a1e7146a121b8df76509ff4f1972b70ad3866
Ayala-Ruan Cesar <69020800+Ayala-Ruan-CesarM@users.noreply.github.com>
2026-02-13 10:21:42 -0500

    Update microbial_network.R to include plotting functions
    Commented out print and saveRDS statements for phyloseq object. Added code to generate network plots and legends for figures in the manuscript.

--- microbial_network.R
+++ microbial_network.R
@@ -61,11 +61,11 @@ for (file in file_paths) {
   assign(object_name, phyloseq_object_test)
 
 }
-print(phyloseq_object_test)
+#print(phyloseq_object_test)
 
 # Subset the phyloseq object to only include the PW samples without QC filterd
 physeq_subset_PW_200 <- subset_samples(phyloseq_object_test, Treatment == "PW")
-saveRDS(physeq_subset_PW_200, "Simulations_genera_level/physeq_subset_PW_200")
+#saveRDS(physeq_subset_PW_200, "Simulations_genera_level/physeq_subset_PW_200")
 physeq_filtered_PW <- filter_taxa(physeq_subset_PW_200, function(x) sum(x > 0) > (0.2 * length(x)), TRUE)
 
 # Construct the network using the SPRING method
@@ -88,10 +88,83 @@ net_spring_high_820freq_rep100  <- netConstruct(
   verbose = 3,
   seed = 57)
 
-net_spring_high_820freq_rep100 <- readRDS("Simulations_genera_level/net_spring_high_820freq_rep100")
 net_analyzed_820_f2 <- netAnalyze(net_spring_high_820freq_rep100,
                                   entrLCC = TRUE,
                                   clustMethod = "cluster_fast_greedy",
                                   hubPar = "eigenvector",
                                   weightDeg = FALSE, 
                                   normDeg = FALSE) 
+
+################ All Taxa plot. Annex Fig. ################
+                                  
+png("FigAnnex_network_820.png", 
+    width = 2000, 
+    height = 1600, 
+    res = 300)
+                                  
+ p_F2_820 <- plot(net_analyzed_820_f2,
+                 edgeWidth = 0.05,
+                 edgeTranspLow = 50,
+                 edgeTranspHigh = 50, # THe higher the more transparent it is
+                 labels = FALSE,
+                 rmSingles = TRUE)
+dev.off()
+################ Fig. 5 on the manuscript ################
+png("Fig5_network_820.png", 
+    width = 2000, 
+    height = 1600, 
+    res = 300)
+                                  
+p_F2_820 <- plot(net_analyzed_820_f2,
+                 nodeFilter = "highestDegree",
+                 nodeFilterPar = 75,
+                 nodeSize = "degree",
+                 edgeWidth = 1,
+                 edgeTranspLow = 50,
+                 edgeTranspHigh = 20, # THe higher the more transparent it is
+                 cexLabels = 2.0,
+                 cexNodes = 1.5,
+                 labels = TRUE,
+                 rmSingles = TRUE,
+                 mar = c(1, 3, 4, 8)
+                ) 
+
+legend(0.8, 0.9, cex = 0.5 , title = "Estimated association:",
+       legend = c("+","-"), lty = 1, lwd = 3, col = c("#009900","red"), 
+       bty = "n", horiz = TRUE)
+
+#1. Get the cluster IDs and node colors
+cluster_data <- net_analyzed_820_f2$clustering$clust1
+plot_colors <- p_F2_820$nodecolor$nodecol1
+
+# 2. Get the names of the nodes that were plotted
+plotted_node_names <- names(plot_colors)
+
+# 3. Filter the cluster data to include only the nodes that were plotted
+aligned_cluster_data <- cluster_data[match(plotted_node_names, names(cluster_data))]
+
+# 4. Create a data frame to combine the aligned vectors
+node_info <- data.frame(
+  cluster_id = aligned_cluster_data,
+  color = plot_colors
+)
+
+# 5. Get the unique cluster IDs and their corresponding colors
+legend_data <- node_info[!duplicated(node_info$cluster_id), ]
+
+# Order the legend data by cluster ID
+legend_data <- legend_data[order(legend_data$cluster_id), ]
+
+# Get the final lists for the legend function
+cluster_names <- paste("Cluster", legend_data$cluster_id)
+cluster_colors <- legend_data$color
+
+legend(0.9, 0.15,
+       title = "Clusters",
+       legend = cluster_names,
+       col = cluster_colors,
+       pch = 19,
+       bty = "n",
+       cex = 0.6)
+
+dev.off()
